8f3a45c02ba651bb59cd96acfca2ea452a0a1630
Chris131312 <christophermoli13@gmail.com>
2026-03-30 01:15:53 -0400

    feat: create POST endpoint to register new gym members

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-require("./config/db");
+const pool = require("./config/db");
 
 const app = express();
 
@@ -13,6 +13,29 @@ app.get("/api/health", (req, res) => {
     success: true,
     message: "Gym Management API is running smoothly.",
   });
+});
+
+app.post("/api/members", async (req, res) => {
+  try {
+    const { first_name, last_name, email, phone_number } = req.body;
+
+    const newMember = await pool.query(
+      "INSERT INTO members (first_name, last_name, email, phone_number) VALUES ($1, $2, $3, $4) RETURNING *",
+      [first_name, last_name, email, phone_number],
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Member registered successfully!",
+      data: newMember.rows[0],
+    });
+  } catch (error) {
+    console.error("Error saving member:", error.message);
+    res.status(500).json({
+      success: false,
+      error: "Internal server error",
+    });
+  }
 });
 
 app.listen(PORT, () => {
