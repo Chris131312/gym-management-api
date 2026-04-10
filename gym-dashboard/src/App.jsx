ff56cfde8a5743e175a3d1d62f09c40c17e06f19
Chris131312 <christophermoli13@gmail.com>
2026-04-10 11:42:19 -0400

    feat: add history members in gym

--- gym-dashboard/src/App.jsx
+++ gym-dashboard/src/App.jsx
@@ -74,6 +74,21 @@ function App() {
     }
   };
 
+  const fetchCheckins = async () => {
+    setIsLoadingCheckins(true);
+    try {
+      const response = await fetch("http://localhost:3000/api/checkins");
+      const data = await response.json();
+      if (response.ok) {
+        setRecentCheckins(data.data || []);
+      }
+    } catch (error) {
+      console.error("Error fetching members:", error);
+    } finally {
+      setIsLoadingMembers(false);
+    }
+  };
+
   const fetchMembers = async () => {
     setIsLoadingMembers(true);
     try {
