e4d0b52e62a8e8d8021f264a65e171c1e61f5a19
Christopher Molina <christophermoli13@gmail.com>
2026-05-11 10:37:43 -0400

    feat(backend): - Add errorHandler middleware for centralized error responses

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,4 +1,8 @@
-const AppError = require("..utils/AppError")
-function errorHandler(err, req,res,next) {
-    let error = err
+const AppError = require("..utils/AppError");
+function errorHandler(err, req, res, next) {
+  let error = err;
+
+  console.error("Error:", {
+    message: err.message,
+  })
 }
