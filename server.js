c7bf4516222c5dfe2f99eca629ff0e77c7985dfd
Chris131312 <christophermoli13@gmail.com>
2026-05-14 00:13:42 -0400

    refactor(backend): apply asyncHandler and custom errors to all routes

--- server.js
+++ server.js
@@ -16,6 +16,65 @@ const {
   updatedMemberSchema,
 } = require("./schemas/memberSchema");
 const { NotfoundError, ForbiddenError } = require("./utils/errors");
+const { success } = require("zod");
+const { is } = require("zod/locales");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+//Health Check
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Gym Management API is running smoothly.",
+  });
+});
+
+//CREATE MEMBER
+app.post(
+  "/api/members",
+  validate(createMemberSchema),
+  asyncHandler(async (req, res) => {
+    const { first_name, last_name, email, phone_number, is_active } = req.body;
+
+    const newMember = await pool.query(
+      "INSERT INTO members (first_name, last_name, email, phone_number, is_active) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [first_name, last_name, email, phone_number, is_active],
+    );
+    res.status(201).json({
+      success: true,
+      message: "Member registered successfully!",
+      data: newMember.rows[0],
+    });
+  }),
+);
+
+// READ ALL MEMBERS
+app.get(
+  "/api/members",
+  asyncHandler(async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+
+    const countResult = await pool.query("SELECT COUNT(*) FROM members");
+    const totalMembers = parseInt(countResult.rows[0].count);
+
+    const allMembers = await pool.query(
+      "SELECT * FROM members ORDER BY created_at DESC LIMIT $1 OFFSET $2",
+      [limit, offset],
+    );
+
+    res.status(200).json({
+      success: true,
+      limit: limit,
+      page: page,
+      totalMembers: totalMembers,
+      totalPages: Math.ceil(totalMembers / limit),
+      data: allMembers.rows,
+    });
+  }),
+);
