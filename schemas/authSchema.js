7bc89458791ed264039cc7447ae7284d1c621b3a
Christopher Molina <christophermoli13@gmail.com>
2026-05-31 09:16:16 -0400

    feat(auth): add Zod validation schemas for register and login

--- schemas/authSchema.js
+++ schemas/authSchema.js
@@ -7,4 +7,9 @@ const registerSchema = Z.object({
     .toLowerCase()
     .email({ error: "Invalid email formar" })
     .max(255, { error: "Email is too long" }),
+
+  password: z
+    .string({ error: "Password is required" })
+    .min(8, { error: "Password must be at least 8 characters" })
+    .max(72, { error: "Password must be at most 72 characters" }),
 });
