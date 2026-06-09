944e1c5a2350690b3ba085c5a996194c2ee223eb
Christopher Molina <christophermoli13@gmail.com>
2026-06-09 12:34:38 -0400

    feat(auth): add protect and restrictTo middleware

--- middleware/protect.js
+++ middleware/protect.js
@@ -4,4 +4,10 @@ const { UnauthorizedError } = require("../utils/errors");
 
 const protect = async (req, res, next) => {
   const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startWith("Bearer")) {
+    throw new UnauthorizedError("No token provided. Please log in");
+  }
+
+  const token = authHeader.split(" ")[1];
 };
