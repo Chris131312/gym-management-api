3f078e5b72136ebc76deadb5d3d598cdede64729
Christopher Molina <christophermoli13@gmail.com>
2026-06-26 01:16:37 -0400

    feat(staff): add staff management page with user registration

---
+++ gym-dashboard/src/components/StaffPage.jsx
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from "react";
+import { api } from "../api/client";
+import { Shield, UserPlus } from "lucide-react";
+import RegisterUserModal from "./RegisterUserModal";
